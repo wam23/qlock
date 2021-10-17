3202d7ac93fc5c2d26f1babbec07970043087ac5
Rafael Wampfler <wampfler@gmail.com>
2021-10-17 12:17:15 +0200

    fix compile password in header

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,40 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include "secret.h" // ssid and password
 /*
  * ESP8266
  * https://makesmart.net/esp8266-http-get-request/
  * http://worldtimeapi.org/api/timezone/Europe/Zurich
  */
-
-HTTPClient sender;
-
-const char *ssid = "WLAN_SSID";
-const char *password = "WLAN_PASSWD";
+WiFiClient client;
+HTTPClient http;
 
 int hour = 0;
-int min = 0;
-int sec = 0;
+int minute = 0;
+int second = 0;
 
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  String datetime = "2021-10-14T22:03:52.040778+02:00";
-  String hour = datetime.substring(11, 13);
-  String min = datetime.substring(14, 16);
-
-  Serial.println(hour + ":" + min);
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  updateSeconds();
-  updateTime();
-}
-
-void updateSeconds();
+void updateSeconds()
 {
   static const unsigned long REFRESH_INTERVAL = 1000; // ms
   static unsigned long lastRefreshTime = 0;
@@ -42,15 +21,15 @@ void updateSeconds();
   if (millis() - lastRefreshTime >= REFRESH_INTERVAL)
   {
     lastRefreshTime += REFRESH_INTERVAL;
-    sec++;
-    if (sec >= 60)
+    second++;
+    if (second >= 60)
     {
-      sec = 0;
-      min++;
+      second = 0;
+      minute++;
     }
-    if (min >= 60)
+    if (minute >= 60)
     {
-      min = 0;
+      minute = 0;
       hour++;
     }
     if (hour >= 24)
@@ -84,29 +63,42 @@ void updateWebTime()
   }
   Serial.println("Connected");
 
-  if (sender.begin("http://worldtimeapi.org/api/timezone/Europe/Zurich"))
+  if (http.begin(client, "http://worldtimeapi.org/api/timezone/Europe/Zurich"))
   {
 
-    int httpCode = sender.GET();
+    int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK)
     {
-      String payload = sender.getString();
+      String payload = http.getString();
       Serial.println(payload);
       int start = payload.indexOf("datetime");
-      int hour = payload.substring(22, 24);
-      int min = payload.substring(25, 27);
-      int sec = payload.substring(28, 30);
+      int hour = payload.substring(22, 24).toInt();
+      int minute = payload.substring(25, 27).toInt();
+      int second = payload.substring(28, 30).toInt();
     }
     else
     {
-      Serial.printf("HTTP-Error: ", sender.errorToString(httpCode).c_str());
+      Serial.printf("HTTP-Error: ", http.errorToString(httpCode).c_str());
     }
 
-    sender.end();
+    http.end();
     Serial.println("Disconnected");
   }
   else
   {
-    Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
+    Serial.printf("HTTP failed");
   }
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  updateSeconds();
+  updateTime();
 }
